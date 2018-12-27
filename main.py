e055b4fa363bfb41a369c2438c3f3074322ce936
Alex Reynen <alexreynen@gmail.com>
2018-12-27 00:38:34 -0800

    Get all the music working to show textually the fines and such, also handles showing were tagging errors exist, such as when there is no albumArtist, where it'll replace it with the artist.  There are some songs where both the album artist and the artist dont exist, here itll defualt to being "tagging error" so we can the easily see in the file system what must be done

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from os import listdir
 from os.path import join, isdir, dirname, abspath
-
+from mutagen.mp3 import MP3
+from mutagen.easyid3 import EasyID3
+from mutagen.flac import FLAC
+import mutagen.id3
 
 class List:
     masterList = []
+    song_count = 0
+    not_count = 0
 
     def add(self, file, path):
-        s = SongFile(file, path)
+        type = file[-4:].replace(".", "")
+        if type == "flac" or type == "mp3":
+            # print("is song")
+            # self.song_count+=1
+            song = SongFile(file, path, type)
+            self.masterList.append(song)
+        else:
+            # print("not song")
+            # self.not_count+=1
+            pass
 
     def makeMasterList(self, path):
         files = listdir(path)
@@ -18,41 +32,82 @@ class List:
 
     def printMasterList(self):
         for f in self.masterList:
-            print(f)
-
-    def writeMasterList(self):
-        file = open("enumeration.txt", "w")
-        for i in range(self.masterList.__len__()-1):
-            file.write(self.masterList[i] + "\n")
-        try:
-            file.write(self.masterList[-1])
-        except:
-            print("No Song files")
+            f.printSong()
+        # print(self.song_count)
+        # print(self.not_count)
 
 
 class SongFile:
-    title = ""
-    albumArtist = ""
-    album = ""
-    track = -1
-    path = ""
-    def __init__(self, song, path):
-        self.path = path
-        self.title = ""
-        self.albumArtist = ""
-        self.album = ""
-        self.track = -1
+    title = "Tagging Error"
+    albumArtist = "Tagging Error"
+    album = "Tagging Error"
+    track = "-1"
+    path = "Tagging Error"
+
+    def __init__(self, song, path, type):
+        self.path = path + "\\" + song
+        if type == "flac":
+            metaData = FLAC(self.path)
+            try:
+                self.title = metaData["title"][0]
+            except:
+                print("***" + song + "Title Error")
+            try:
+                self.albumArtist = metaData["albumartist"][0]
+            except:
+                print("***" + song + "AlbumArtist Error")
+                try:
+                    self.albumArtist = ''.join(metaData["artists"])
+                    print("***" + "But the artist works")
+                except:
+                    print("*** AND" + song + " Artist Error")
+            try:
+                self.album = metaData["album"][0]
+            except:
+                print("***" + song + "Album Error")
+            try:
+                self.track = metaData["tracknumber"][0].split('/')[0]
+            except:
+                print("***" + song + "Track number Error")
+        elif type == "mp3":
+            metaData = MP3(self.path)
+            try:
+                self.title = ''.join(metaData["TIT2"])
+            except:
+                print("***" + song + "Title Error")
+            try:
+                self.albumArtist = ''.join(metaData["TPE2"])
+            except:
+                print("***" + song + " AlbumArtist Error")
+                try:
+                    self.albumArtist = ''.join(metaData["TPE1"])
+                except:
+                    print("***" + song + " Artist Error")
+            try:
+                self.album = ''.join(metaData["TALB"])
+            except:
+                print("***" + song + "Album Error")
+            try:
+                self.track = ''.join(metaData["TRCK"]).split('/')[0]
+            except:
+                print("***" + song + "Track number Error")
+
+    def printSong(self):
+        print("title : " + self.title
+                + "; albumArtist : " + self.albumArtist
+                + "; album : " + self.album
+                + "; track : " + self.track)
 
 
 
 def main():
     l = List
-    path = input("What is the path to enumerate? ('q' for the directory of the python file): ")
+    path = input("What is the path to work with? ('q' for the directory of the python file): ")
     if path == 'q':
         path = dirname(abspath(__file__))
+    # path = "D:\Music\Childish Gambino"
     l.makeMasterList(l, path)
     l.printMasterList(l)
-    l.writeMasterList(l)
     # filesAndDirs = listdir()
     # l.fillOutFiles(filesAndDirs)
     # print(filesAndDirs)
